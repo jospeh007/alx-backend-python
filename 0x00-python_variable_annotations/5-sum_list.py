acfb2c7fd42ec09d01a66df2bb298acbd3c6a63d
Youssef Rhaimouni <youssefgf123@gmail.com>
2024-07-04 07:19:33 -0700

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Complex types - list of floats
+"""
+
+
+def sum_list(input_list: float) -> sum:
+    """
+    Returns their sum as a float
+    """
+    return sum(input_list)
