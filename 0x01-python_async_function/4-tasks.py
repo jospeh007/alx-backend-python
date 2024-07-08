5629d48559c3d56332aef93f007d571490e9c2a2
Youssef Rhaimouni <youssefgf123@gmail.com>
2024-07-08 07:48:04 -0700

    Task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,8 +6,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ The code is nearly identical to wait_n except 
-    task_wait_random is being called """
+    """ Tasks 4 """
     futures = [task_wait_random(max_delay) for _ in range(n)]
     futures = asyncio.as_completed(futures)
     delays = [await future for future in futures]
